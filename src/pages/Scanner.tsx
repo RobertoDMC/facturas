8749ad70fae7c75719fb77601c15ad55fc88ba40
friendou <robert.dmc@hotmail.com>
2021-10-24 00:28:02 -0400

    Added rescan funtionallity

--- src/pages/Scanner.tsx
+++ src/pages/Scanner.tsx
@@ -1,26 +1,31 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonPage, IonRouterLink, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import { QRScanner, QRScannerStatus } from "@ionic-native/qr-scanner";
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { addBill } from '../store/bills/billsSlice';
 import { useDispatch } from 'react-redux';
-import { Bill } from '../classes/bill';
+import { useHistory } from "react-router-dom";
 import { arrowBack } from 'ionicons/icons';
 import './Scanner.css';
+import { Dialogs, DialogsPromptCallback } from '@ionic-native/dialogs';
+import { Toast } from '@ionic-native/toast';
 
 const Scanner: React.FC = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const openScanner = async () => {
-    const x: string = "1009445021|17480|263401100172312|12/10/2021|31.83|31.83|90-CD-65-07-28|5225670|0|0|0|0"
-    const bill = Bill.getBill(x);
-    dispatch(addBill(bill));
-    /* const status: QRScannerStatus = await QRScanner.prepare();
-    if (status.authorized) {
-      let scan = QRScanner.scan().subscribe((text: string) => {
-        dispatch(addBill(text));
-        addBill(text);
-        scan.unsubscribe();
+    const data = await BarcodeScanner.scan();
+    dispatch(addBill(data.text));
+    Dialogs.prompt('Desea scanear mas facturas?', 'Continuar?', ['Continuar', 'Cancelar'])
+      .then((result: DialogsPromptCallback) => {
+        if (result.buttonIndex === 1) {
+          openScanner();
+        } else {
+          history.goBack();
+        }
       })
-    } */
+      .catch(() => {
+        Toast.show('Hubo un problema inesperado', '1500', 'bottom');
+      });
   };
 
   return (
@@ -28,13 +33,13 @@ const Scanner: React.FC = () => {
       <IonHeader>
         <IonToolbar>
           <IonGrid>
-            <IonRow className="ion-align-items-center">
-              <IonCol size="2">
-                <IonItem routerLink="/home">
+            <IonRow className='ion-align-items-center'>
+              <IonCol size='2'>
+                <IonItem routerLink='/home'>
                   <IonIcon icon={arrowBack} />
                 </IonItem>
               </IonCol>
-              <IonCol size="10">
+              <IonCol size='10'>
                 <IonTitle>Scanner</IonTitle>
               </IonCol>
             </IonRow>
