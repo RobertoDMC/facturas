3802b76ca433ac50b0041b5a7137178450e98291
friendou <robert.dmc@hotmail.com>
2021-10-24 00:26:00 -0400

    Added swipe gesture for deletion of a bill

--- src/components/BillItem.tsx
+++ src/components/BillItem.tsx
@@ -1,22 +1,72 @@
 import { IonGrid, IonCol, IonRow, IonItem } from '@ionic/react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Bill, BillsSlice } from '../interfaces';
-import { clearAllBills } from '../store/bills/billsSlice';
-import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
-import './Home.css';
+import { Bill } from '../interfaces';
+import { createGesture, Gesture, GestureDetail } from '@ionic/core';
+import { useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBill } from '../store/bills/billsSlice';
 
 interface BillItemProps {
   bill: Bill;
 };
 
+
 const BillItem: React.FC<BillItemProps> = ({ bill }: BillItemProps) => {
+
+  const itemRef = useRef<any>(null);
+
+  const dispatch = useDispatch();
+
+  let gesture: Gesture;
+
+  useEffect(() => {
+    if (itemRef !== null) {
+      gesture = createGesture({
+        el: itemRef.current,
+        threshold: 15,
+        gestureName: 'swipe',
+        onStart: ev => onMoveStart(ev),
+        onMove: ev => onMoveHandler(ev),
+        onEnd: ev => onMoveEnd(ev),
+      });
+      gesture.enable();
+    }
+  }, [itemRef]);
+
+  const onMoveStart = (detial: GestureDetail) => {
+
+  };
+  
+  const onMoveEnd = (detail: GestureDetail) => {
+    if (detail.deltaX > 150) {
+      dispatch(removeBill(bill));
+    } else {
+      itemRef.current.style.transform = `translateX(0px)`;
+      itemRef.current.style.background = '#FFFFFF';
+    }
+  };
+
+  const onMoveHandler = (detail: GestureDetail) => {
+    if (detail.deltaX > 0) {
+      itemRef.current.style.transform = `translateX(${detail.deltaX}px)`;
+      itemRef.current.style.background = getRedBackgroundColor(detail.deltaX);
+    }
+  };
+
+  const getRedBackgroundColor = (deltaX: number) => {
+
+    if (deltaX > 150) {
+      return '#FF0000';
+    } else {
+      return `#FF${(255 - Math.round(deltaX)).toString(16)}${(255 - Math.round(deltaX)).toString(16)}`;
+    }
+  };
 
   return (
     <IonItem>
       <IonGrid>
-        <IonRow className="ion-align-items-center">
+        <IonRow className="ion-align-items-center" ref={itemRef}>
           <IonCol size="1">
-            {Number(bill.n) + 1}
+            {bill.n}
           </IonCol>
           <IonCol size="11">
             <IonRow><IonCol>NIT: {bill.nit}</IonCol></IonRow>
